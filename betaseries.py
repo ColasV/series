b41bab3909c05a56e71c10cfc45b63540a20fcde
Nicolas Vignes <nico.vignes@gmail.com>
2013-08-14 22:23:44 +0100

    Add some Class to organize the code Add some get method to get title, duration etc.

--- betaseries.py
+++ betaseries.py
@@ -1,8 +1,83 @@
+# Important stuff
+
 import urllib2
 import json
 import logging
 import os
 import zipfile
+
+# Utilities function
+
+def convert(input):
+    if isinstance(input, dict):
+        return {convert(key): convert(value) for key, value in input.iteritems()}
+    elif isinstance(input, list):
+        return [convert(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
+
+
+# Class Show
+# Object is using by the BetaSeries class
+class Show():
+	# Init your object
+	# Need data from the show
+	def __init__(self,show_info):
+		self._show_info = show_info
+
+	def _get_title(self):
+		return self._show_info['title']
+
+	def _get_description(self):
+		return convert(self._show_info['description'])
+
+	def _get_duration(self):
+		return self._show_info['duration']
+
+	def _get_banner(self):
+		return self._show_info['banner']
+
+
+	description = property(_get_description)
+	title = property(_get_title)
+	duration = property(_get_duration)
+	banner = property(_get_banner)
+
+# Class Subtitle
+# Using to create subtitle object
+# Allow to download the subtitle and get informations
+class Subtitle():
+
+	def __init__(self,subtitle_info):
+		self._subtitle = subtitle_info
+
+	def _get_name(self):
+		return self._subtitle['file']
+
+	def download(self):
+		url = self._subtitle['url']
+		name = self._subtitle['file']
+		try:
+			f = urllib2.urlopen(url)
+			# Open our local file for writing
+			with open(os.path.basename(name), "wb") as local_file:
+				local_file.write(f.read())
+		except Exception as err:
+			logging.error('Error during downloading :' + str(err))
+		try:
+			# Create a ZipFile object
+			# Only 1 file per Zip
+			zf = zipfile.ZipFile(name)
+			zf.extract(zf.namelist()[0])
+		except Exception as err:
+			logging.error('Error during opening archive :' + str(err))
+
+
+	name = property(_get_name)
+
+
 
 class BetaSeries():
 
@@ -39,67 +114,46 @@ class BetaSeries():
 
 	# Get Data from a Serie using url name
 	# Return a dictionnary with multiple information
-	def getSerie(self,show_url):
+	def getShow(self,show_url):
 		url = 'http://api.betaseries.com/shows/display/' + str(show_url) + '.json?key=' + str(self._key)
 		response = urllib2.urlopen(url)
 		html = response.read()
+		html = html
 		data = json.loads(html)
 
-		show_info = {}
-
 		try:
-			for i in data['root']['show']:
-				show_info[i] = data['root']['show'][i]
+			show = Show(data['root']['show'])
 
 			if self._verbose:
-				for info in show_info:
-					print(info)
+				logging.info('*Show object correctly created')
 
-			return show_info
+			return show
 
 		except Exception as err:
 			logging.error('Error : ' + str(err))
 
-	# Get a list of subtitle		
+	# Get a dictionnaries of subtitle		
 	# Need url show, season,nb_numero and language
-	def getSubtitle(self,show_url,season,nb_episode,language):
+	def getSubtitle(self,show_url,season,nb_episode,language='VO'):
 		url = 'http://api.betaseries.com/subtitles/show/' + str(show_url) + '.json?key=' + str(self._key) + '&language=' + str(language) + '&season=' + str(season) + '&episode=' + str(nb_episode)
 		response = urllib2.urlopen(url)
 		html = response.read()
 		data = json.loads(html)
 
-		subtitle = {}
+		subtitles = {}
 
 		try:
 			for i in data['root']['subtitles']:
-				subtitle[i] = data['root']['subtitles'][i]
+				subtitles[i] = Subtitle(data['root']['subtitles'][i])
 
 			if self._verbose:
-				for info in subtitle:
+				for info in subtitles:
 					print(info)
 
-			return subtitle
+			return subtitles
 
 		except Exception as err:
 			logging.error('Error : ' + str(err))
 
-	# Dowload a subtitle and extract it in a folder
-	# Need a subtitle_object
-	def dowloadSubtitle(self,subtitle_object):
-		url = subtitle_object['url']
-		name = subtitle_object['file']
-
-		if self._verbose:
-			print("downloading " + str(url))
-
-		f = urllib2.urlopen(url)
-		# Open our local file for writing
-		with open(os.path.basename(name), "wb") as local_file:
-			local_file.write(f.read())
-
-		# Create a ZipFile object
-		# Only 1 file per Zip
-		zf = zipfile.ZipFile(name)
-		zf.extract(zf.namelist()[0])
 
 
