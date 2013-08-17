313b86087a9bb356533647985fc1a834f5f0d329
Nicolas Vignes <nico.vignes@gmail.com>
2013-08-17 15:24:45 +0100

    Add some examples

--- example1.py
+++ example1.py
@@ -1,33 +1,38 @@
-# Example 1 :
-# Take a directory path
-# For each folder, check if the name exist on the database (same name)
-# Produce a html report with all the shows + description + banner
+# Example 1:
+# How to use the API
 
+# Import the library
 from betaseries import *
-import os
 
-B = BetaSeries('')
-new_dir = r'/Volumes/DD2/Videos/Series/'
+# Create an object with the API key
+B = BetaSeries('245d8c3b4a91')
 
-listF = os.listdir(new_dir)
+# Search for a serie in the database:
+# Look at all the shows with the keyword
+result = B.search('NCIS')
+for i in result:
+	print(i['title'])
 
-F = open('content.html','w')	
-F.write('<html><head><meta charset="utf-8"></head><body>')
-for i in listF:
-	new_p = new_dir + i
-	
-	if not os.path.isfile(new_p):
-		answer = B.searchSpecific(i)
-		
-		if answer != None:
-			# Print debug 
-			print(answer['title'])
+print("\n")
 
-			# Write in html file
-			F.write('<h1>' + str(answer['title']) + '</h1>')
-			val = B.getShow(answer['url'])
-			F.write('<p>' + str(val.description) + '</p>')
-			F.write('<img src="' + str(val.banner) + '" />')
+# Specific search:
+# return a dictionnary with two element url and title
+result_specific = B.searchSpecific('NCIS')
+print(result_specific['title'])
 
-F.write('</body></html>')
-F.close()
+# Get a show informations
+# Need a show url
+# Return a Show object
+result_show = B.getShow(result_specific['url'])
+print('Title : ' + result_show.title + "\n")
+print('Description : ' + result_show.description)
+
+# Download a subtitle
+# Need the url show, season number, episode number and optionnal (VO|VF)
+result_subtitle = B.getSubtitle(result_specific['url'],1,1,'VO')
+
+# Get a list of Subtitle object
+# Try to download the first one
+# Download and unzip in your script folder
+print ('Downloading : ' + result_subtitle[0].name)
+result_subtitle[0].download()
