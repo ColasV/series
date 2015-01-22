84f172af5217ffb9ada99fbcbc7ee76c6e4943a2
Nicolas Vignes <nicolas.vignes@ensimag.grenoble-inp.fr>
2015-01-22 09:34:08 +0100

    Adding subdirectory test

---
+++ tests/test_extension.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from lib.betaseries import extension
+
+__author__ = 'vignesn'
+
+
+class TestExtension(TestCase):
+    def test_extension_zip(self):
+        filename = 'fichier.zip'
+        self.assertEqual('zip', extension(filename))
+
+    def test_none_extension(self):
+        filename = 'fichier'
+        self.assertEqual(None, extension(filename))
+
+    def test_multiple_extension(self):
+        filename = 'fichier.test.zip.tar'
+        self.assertEqual('tar', extension(filename))
+
+    def test_path_extension(self):
+        filename = '/path/to/a/file.tar'
+        self.assertEqual('tar', extension(filename))
+
+    def test_null_path(self):
+        filename = ''
+        self.assertEqual(None, extension(filename))
